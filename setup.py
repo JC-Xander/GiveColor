f7787d35e4498de9a21888827347630d5189f356
JC-Xander <j.xanderoficial@gmail.com>
2024-10-07 00:56:51 -0600

    Version preliminar del proyecto

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='GiveColor',
+    name='givecolor',
     version='0.1.0',
     author='JC-Xander',
     author_email='j.xanderoficial@gmail.com',
@@ -9,14 +9,13 @@ setup(
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/JC-Xander/GiveColor',
-    download_url='https://github.com/JC-Xander/GiveColor/archive/refs/tags/0.1.0.zip',
-    keywords=['Color', 'Terminal', 'font'],
+
     packages=find_packages(),
     install_requires=[],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: Windows',
+        'Operating System :: Microsoft :: Windows',
     ],
     python_requires='>=3.6',
 )
