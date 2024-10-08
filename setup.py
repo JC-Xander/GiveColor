aa8c3fb4eb8eddc98154e0730b4556db2278a6bc
JC-Xander <j.xanderoficial@gmail.com>
2024-10-07 22:46:20 -0600

    Ajustando versión del proyecto

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='givecolor',
-    version='0.1.3',
+    version='0.1.4',
     author='JC-Xander',
     author_email='j.xanderoficial@gmail.com',
     description='Un paquete para imprimir texto de colores en la terminal de Windows',
